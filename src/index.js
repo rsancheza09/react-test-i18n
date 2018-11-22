48bd9c9fe0708b1451750293cb32e403e324b7c9
Randall Sánchez A <rsancheza24@gmail.com>
2018-11-22 10:12:31 -0600

    Initial commit from Create React App

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import {Route} from 'react-router-dom';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
@@ -24,3 +25,16 @@ render(
   target
 );
 registerServiceWorker();
+=======
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
+>>>>>>> Initial commit from Create React App
