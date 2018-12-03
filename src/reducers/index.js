81eebb1f4a0ef6e69060470bdb6aacedf0203e45
Randall Sánchez A <rsancheza24@gmail.com>
2018-12-02 22:01:37 -0600

    setting up i18n and material ui

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
+import { i18nReducer } from 'react-redux-i18n';
 import counter from './counter';
 
 export default (history) => combineReducers({
   router: connectRouter(history),
+  i18n: i18nReducer,
   counter,
 });
